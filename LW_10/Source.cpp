41aceb8f4ae14f6e02ecfcd9c31565b1e760cae4
rek655689 <e.gladneva1357@gmail.com>
2023-05-03 20:38:47 +0300

    add lw

---
+++ LW_10/Source.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <thread>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+
+    thread t1([&]()
+        {
+            ifstream file("text1.txt");
+            long int count = 0;
+            char sym;
+
+            while (file >> sym)
+            {
+                count += sym;
+            }
+
+            cout << "Контрольная сумма файла 1: " << count << endl;
+        });
+
+    thread t2([&]()
+        {
+            ifstream file("text2.txt"); 
+            long int count = 0;
+            char sym;
+
+            while (file >> sym)
+            {
+                count += sym;
+            }
+
+            cout << "Контрольная сумма файла 2: " << count << endl;
+        });
+
+    t1.join();
+    t2.join();
+
+	return 0;
+}
